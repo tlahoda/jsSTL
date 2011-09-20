eea534504ccdca3374cce58d49cf624cec71130f
Thomas Lahoda <tlahoda@gmail.com>
2011-09-20 15:20:36 -0400

    first commit

---
+++ adjacent_find.js
@@ -0,0 +1,35 @@
+/**
+ * @file adjacent_find.js, Contains the adjacent_find implementation.
+ *
+ * Copyright (C) 2011 Thomas P. Lahoda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+Array.prototype.adjacent_find = function (begin, end, predicate) {
+  if (predicate == undefined) 
+    predicate = function (ele1, ele2) { 
+      return ele1 == ele2;
+    };
+
+  for (var i = begin, last = end - 1; i < last; ++i)
+    if (predicate (this[i], this[i + 1]))
+      return i;
+  return end;
+}
+
