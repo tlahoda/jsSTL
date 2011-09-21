9ef3f3227bfcaaa7e75dcc14bf0a11303fd15385
Thomas Lahoda <tlahoda@gmail.com>
2011-09-21 14:42:16 -0400

    Added search and search_if

---
+++ algorithms/search_if.js
@@ -0,0 +1,39 @@
+/**
+ * @file search_if.js, Contains the search_if implementation.
+ *
+ * Copyright (C) 2011 Thomas P. Lahoda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+Array.prototype.search_if = function (begin1, end1, array2, begin2, predicate) {
+  var len = end2 - begin2;
+  for (; begin1 < end1; ++begin1) {
+    var matches = 0;
+    for (var j = begin2; j < end2; ++j)
+      if (predicate (this[begin1], array2[j]))
+        ++matches;
+      else
+        break;
+
+    if (matches == length)
+      break;
+  }
+  return begin1;
+}
+
