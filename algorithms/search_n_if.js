e90d0006e2109e07e6989d4f3a413508cfc9ce34
Thomas Lahoda <tlahoda@gmail.com>
2011-09-21 15:02:05 -0400

    Added search_n and search_if algorithms.

---
+++ algorithms/search_n_if.js
@@ -0,0 +1,39 @@
+/**
+ * @file search_n_if.js, Contains the search_n_if implementation.
+ *
+ * Copyright (C) 2011 Thomas P. Lahoda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+Array.prototype.search_n_if = function (begin, end, n, value, predicate) {
+  var matches = 0;
+  while (matches < n && begin < end) {
+    if (predicate (this[begin], value))
+      ++matches;
+    else
+      matches = 0;
+
+    if (matches == n)
+      break;
+    else
+      ++begin;
+  }
+  return begin;
+}
+
