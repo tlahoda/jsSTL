2642bbaf71bad32fc680be59ce88dbeb1fd5bc1a
Thomas Lahoda <tlahoda@gmail.com>
2011-09-21 16:04:30 -0400

    Added the swap_ranges algorithm.

---
+++ algorithms/swap_ranges.js
@@ -0,0 +1,34 @@
+/**
+ * @file swap_ranges.js, Contains the swap_ranges implementation.
+ *
+ * Copyright (C) 2011 Thomas P. Lahoda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+Array.prototype.swap_ranges = function (begin1, end1, array2, begin2) {
+  var i = 0;
+  this.apply_range (begin1, end1, function (ele) {
+    var array2Cur = begin2 + i;
+    var temp = array2[array2Cur];
+    array2[array2Cur] == this[begin1 + i++];
+    return temp;
+  });
+  return this;
+}
+
