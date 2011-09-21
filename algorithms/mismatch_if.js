270e58bad49ca4a7063d0971d50c9eeff38a1885
Thomas Lahoda <tlahoda@gmail.com>
2011-09-21 13:31:11 -0400

    Added mismatch and mismatch_if algorithms.

---
+++ algorithms/mismatch_if.js
@@ -0,0 +1,31 @@
+/**
+ * @file mismatch.js, Contains the mismatch implementation.
+ *
+ * Copyright (C) 2011 Thomas P. Lahoda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+Array.prototype.mismatch = function (begin1, end1, array2, begin2) {
+  var i = 0;
+  for (var last = end1 - begin1; i < end1 - last; ++i)
+    if (this[begin1 + i] != array2[begin2 + i])
+      break;
+  return new pair (begin1 + i, begin2 + i);
+}
+
