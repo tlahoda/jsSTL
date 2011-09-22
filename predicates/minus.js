af77bdd245eb2a73af430606089097a0ce4e58f5
Thomas Lahoda <tlahoda@gmail.com>
2011-09-22 13:17:34 -0400

    Added divides, minus, modulus, multiplies, negate, and plus predicates.

---
+++ predicates/minus.js
@@ -0,0 +1,33 @@
+/**
+ * @file minus.js, Contains the minus implementation.
+ *
+ * Copyright (C) 2011 Thomas P. Lahoda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+function minus (ele1, ele2) {
+  if (arguments.length == 1)
+    var ele2 = ele1;
+    return function (ele1) {
+      return ele1 - ele2;
+    }
+  else
+    return ele1 - ele2;
+}
+
