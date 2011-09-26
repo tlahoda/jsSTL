3de67cdb1a7df075e73ca17b35066f3b56fa704c
Thomas Lahoda <tlahoda@gmail.com>
2011-09-26 01:35:25 -0400

    Added accumulate, accumulate_if, adjacent_difference, adjacent_difference_if, partial_sum, and partial_sum numeric algorithms.

---
+++ numeric/partial_sum.js
@@ -0,0 +1,32 @@
+/**
+ * @file partial_sum.js, Contains the partial_sum implementation.
+ *
+ * Copyright (C) 2011 Thomas P. Lahoda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+Array.prototype.partial_sum = function (begin, end) {
+  var len = end - begin;
+  var res = new Array (len);
+  var res[0] = this[begin];
+  for (var i = 1; i < len; ++i)
+    res[i] = res[i - 1] + this[i];
+  return res;
+}
+
