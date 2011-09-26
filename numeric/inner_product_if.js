97c2ac7bd993f84f9d575fd5d102aacf2802fc4c
Thomas Lahoda <tlahoda@gmail.com>
2011-09-26 02:43:57 -0400

    Added inner_product and inner_product_if numeric algorithms.

---
+++ numeric/inner_product_if.js
@@ -0,0 +1,32 @@
+/**
+ * @file inner_product_if.js, Contains the inner_product_if implementation.
+ *
+ * Copyright (C) 2011 Thomas P. Lahoda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+Array.prototype.inner_product_if = function (begin1, end1, array2, begin2, init, op1, op2) {
+  var res = init;
+  var i = -1;
+  this.for_each_range (begin1, end1, function (ele) {
+    res = op1 (res, op2 (ele, array2[begin2 + ++i]));
+  });
+  return res;
+}
+
